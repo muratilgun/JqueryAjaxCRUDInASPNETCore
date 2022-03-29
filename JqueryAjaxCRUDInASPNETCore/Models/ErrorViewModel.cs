dbb066055a145a2e1e33d8a4d7dc82ed61eea3ee
muratilgun <muratilgun34@gmail.com>
2022-03-29 23:11:21 +0300

    Initial commit

---
+++ JqueryAjaxCRUDInASPNETCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JqueryAjaxCRUDInASPNETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

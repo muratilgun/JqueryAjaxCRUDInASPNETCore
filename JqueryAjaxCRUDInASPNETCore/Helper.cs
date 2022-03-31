ba5017589603222647977f4c81e86f1ae7d868ea
muratilgun <muratilgun34@gmail.com>
2022-03-31 09:12:50 +0300

    Helper NoDirectAccessAttribute

--- JqueryAjaxCRUDInASPNETCore/Helper.cs
+++ JqueryAjaxCRUDInASPNETCore/Helper.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace JqueryAjaxCRUDInASPNETCore
 {
@@ -26,6 +29,19 @@ namespace JqueryAjaxCRUDInASPNETCore
                 );
                 viewResult.View.RenderAsync(viewContext);
                 return sw.GetStringBuilder().ToString();
+            }
+        }
+
+        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+        public class NoDirectAccessAttribute : ActionFilterAttribute
+        {
+            public override void OnActionExecuting(ActionExecutingContext filterContext)
+            {
+                if (filterContext.HttpContext.Request.GetTypedHeaders().Referer == null || 
+                    filterContext.HttpContext.Request.GetTypedHeaders().Host.ToString() != filterContext.HttpContext.Request.GetTypedHeaders().Referer.Host.ToString())
+                {
+                    filterContext.HttpContext.Response.Redirect("/");
+                }
             }
         }
     }
